e11036a383bcbe212d2836a5ab79ac5232e58c7b
Bikram Maharjan <Bikram Maharjan>
2021-09-06 15:42:16 +0545

    rel

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _fcm = 'Unknown';
+  String _platformVersion = 'Unknown';
 
   @override
   void initState() {
@@ -22,17 +22,25 @@ class _MyAppState extends State<MyApp> {
     initPlatformState();
   }
 
+  // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    String fcm;
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    // We also handle the message potentially returning null.
     try {
-      fcm =
-          await FcmUnrealisticHeartbeat.init ?? 'Unknown platform version';
+      platformVersion =
+          await FcmUnrealisticHeartbeat.platformVersion ?? 'Unknown platform version';
     } on PlatformException {
-      fcm = 'Failed to get platform version.';
+      platformVersion = 'Failed to get platform version.';
     }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
     if (!mounted) return;
+
     setState(() {
-      _fcm = fcm;
+      _platformVersion = platformVersion;
     });
   }
 
@@ -44,7 +52,7 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running : $_fcm\n'),
+          child: Text('Running on: $_platformVersion\n'),
         ),
       ),
     );
