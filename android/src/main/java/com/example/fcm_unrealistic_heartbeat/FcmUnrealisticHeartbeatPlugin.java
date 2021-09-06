bc277e1a83116744e09827ce2f34a2d8fb9ed03c
Bikram Maharjan <Bikram Maharjan>
2021-09-06 13:31:59 +0545

    a

--- android/src/main/java/com/example/fcm_unrealistic_heartbeat/FcmUnrealisticHeartbeatPlugin.java
+++ android/src/main/java/com/example/fcm_unrealistic_heartbeat/FcmUnrealisticHeartbeatPlugin.java
@@ -9,7 +9,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -19,10 +18,6 @@ import io.flutter.embedding.engine.plugins.activity.ActivityAware;
  * FcmUnrealisticHeartbeatPlugin
  */
 public class FcmUnrealisticHeartbeatPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
-    /// The MethodChannel that will the communication between Flutter and native Android
-    ///
-    /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-    /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
     private GcmKeepAlive gcmKeepAlive;
     private Context context;
